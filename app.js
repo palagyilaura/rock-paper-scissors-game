313dce4296c51522061af7defab23c88f173828b
LaraP <palagyilaura@gmail.com>
2021-01-28 21:47:44 +0100

    Game complated

---
+++ app.js
@@ -0,0 +1,116 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    //start the game
+    const startGame = () => {
+        const playBtn = document.querySelector(".intro button");
+        const introScreen = document.querySelector(".intro");
+        const match = document.querySelector(".match");
+
+        playBtn.addEventListener("click", () => {
+            introScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
+        });
+    };
+    //play match
+    const playMatch = () => {
+        const options = document.querySelectorAll(".options button");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+        const hands = document.querySelectorAll(".hands img");
+
+        hands.forEach(hand => {
+            hand.addEventListener("animationend", function () {
+                this.style.animation = "";  
+            });
+        });
+
+        //computer options
+        const computerOptions = ["rock", "paper", "scissors"];
+
+        options.forEach(option => {
+            option.addEventListener("click", function () {
+                //computer choice
+                const computerNumber = Math.floor(Math.random() * 3); 
+                const computerChoice = computerOptions[computerNumber];
+
+                setTimeout(() => {  
+                    
+                    compareHands(this.textContent, computerChoice);
+
+                    //update images
+                    playerHand.src = `./assets/${this.textContent}.png`;
+                    computerHand.src = `./assets/${computerChoice}.png`;
+                }, 2000);
+                //set hand back to rock
+                playerHand.src = './assets/rock.png';
+                computerHand.src = './assets/rock.png';
+                //animation
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease";
+            });
+        });
+    };
+    const compareHands = (playerChoice, computerChoice) => {
+        const winner = document.querySelector(".winner");
+        //checking for tie
+        if (playerChoice === computerChoice) {
+            winner.textContent = "It is a tie";
+            return;
+        }
+        //check for rock
+        if (playerChoice === "rock") {
+            if (computerChoice === "scissors") {
+                winner.textContent = "Player wins";
+                pScore++;          
+                return
+            }
+            else {
+                winner.textContent = "Computer wins";
+                cScore++;               
+                return;
+            }
+        }
+        //check for paper
+        if (playerChoice === "paper") {
+            if (computerChoice === "scissors") {
+                winner.textContent = "Computer wins";
+                cScore++;         
+                return;
+            }
+            else {
+                winner.textContent = "Player wins";
+                pScore++;              
+                return;
+            }
+        }
+        //check for scissors
+        if (playerChoice === "scissors") {
+            if (computerChoice === "rock") {
+                winner.textContent = "Computer wins";
+                cScore++;               
+                return;
+            }
+            else {
+                winner.textContent = "Player wins";
+                pScore++;              
+                return;
+            }
+        }
+    };
+    const updateScore = () => {
+        const playerScore = document.querySelector(".player-score p");
+        const computerScore = document.querySelector(".computer-score p");
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore
+
+    };
+
+    //call all the inner functions
+    startGame();
+    playMatch();
+
+};
+//start the game function
+game();
